a0a40dfc774d124b54b450d00d3d46e513c36cab
demo <abmessi21@gmail.com>
2026-07-15 14:41:23 +0530

    new op

--- calc.js
+++ calc.js
@@ -3,8 +3,12 @@ let b = parseInt(prompt("Enter second number:"));
 
 let sum = a + b;
 let difference = a - b;
+let mul =a*b;
+let div=a/b;
 
 console.log("Addition = " + sum);
 console.log("Subtraction = " + difference);
+console.log("Multiplication = " + mul);
+console.log("Divsion = " + div);
 
-alert("Addition = " + sum + "\nSubtraction = " + difference);
+alert("Addition = " + sum + "\nSubtraction = " + difference +"\mul="+mul+"div="+div);
