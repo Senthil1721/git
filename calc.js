835b667c0e600b2349cfa4893361c8ea3d93e116
demo <abmessi21@gmail.com>
2026-07-15 14:30:45 +0530

    calc

---
+++ calc.js
@@ -0,0 +1,10 @@
+let a = parseInt(prompt("Enter first number:"));
+let b = parseInt(prompt("Enter second number:"));
+
+let sum = a + b;
+let difference = a - b;
+
+console.log("Addition = " + sum);
+console.log("Subtraction = " + difference);
+
+alert("Addition = " + sum + "\nSubtraction = " + difference);
